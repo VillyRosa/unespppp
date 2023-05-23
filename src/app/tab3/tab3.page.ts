acd56cd60a72572e85c9807c76e1ce94bf9527f1
Silvana Caldeira Miranda Betoldo <32404677+Silbetoldo@users.noreply.github.com>
2023-05-22 21:31:46 -0300

    Inserção de mapa

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  @ViewChild('map', { static: false }) mapElement?: ElementRef;
+
   constructor() {}
 
+  ionViewDidEnter() {
+    const mapEle = this.mapElement?.nativeElement;
+
+    if (mapEle) {
+      const map = new google.maps.Map(mapEle, {
+       center: { lat: -21.2084, lng:  -50.4266 }, // Defina o centro do mapa como preferir
+        zoom: 12 // Defina o nível de zoom como preferir
+      });
+
+      const kmlLayer = new google.maps.KmlLayer({
+        url: './src/assets/Meus lugares araçatuba.kml', // Substitua pelo caminho correto para o seu arquivo KML
+        map: map
+      });
+    }
+  }
+  
+ 
 }
